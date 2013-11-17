089ab4c51cbc5b636c21bd52ae4c60267d0e9ae4
itspjc <itspjc@gmail.com>
2013-11-17 20:32:17 +0900

    msg_handler 대폭 수정

--- lssp/server/msg_handler.c
+++ lssp/server/msg_handler.c
@@ -34,63 +34,23 @@ void
 msg_handler()
 {
     char buffer[BLEN]; //4096
-    //char msg [STATUS_MSG_LEN]; /* for status string on response messages */
     int  opcode;
-    //int  r;
-    //u_short seq_num;
     u_short status;
     status = 0;
 
-    while( RTSP_read( buffer, BLEN ) ) // buffer 변수에 in_buffer의 내용이 복사된다.
+    /* in_buffer -> buffer */
+
+    u_short = llen;
+
+    // in_buffer 가 가득찰때까지 기다려야 하는 부분을 만들어야 함
+
+    llen = strcspn (in_buffer, "\r\n"); // 한 줄의 길이를 구함
+    
+    while(llen != 0)
     {
-        /* check for response message.
-         *
-         * valid_response_msg
-         * 어떤 메세지인지 보는 것
-         * 지금은 별로 필요없어서 주석처리 해놓음
-         * Return value :
-         * Request from client - 0, Response from server - 1
-         */
-        //r = valid_response_msg( buffer, &seq_num, &status, msg ); //parse.c
-
-        /* validate_method :
-         * 1. 버퍼 내용을 읽어서 실제적인 파싱이 이루어지는 곳
-         * 2. 리턴되는 opcode를 통해 어떤 명령을 수행해야 될지 handle_event()가 결정
-         */
-
-        opcode = validate_method(buffer); //parse.c
-        
-        /* 프로토콜의 유효성을 검사하는 부분인데 지금은 볼필요 없음
-        if ( !r )
-        {
-            opcode = validate_method( buffer );
-            if ( opcode < 0 )
-            {
-                if ( opcode == -1 )
-                {
-                    assert( sizeof(msg) > 255 );
-                    if ( !sscanf( buffer, " %255s ", msg ) )
-                        strcpy( msg, "[no method encountered]" );
-                    printf( "Method requested was invalid.  Message discarded.  "
-                              "Method = %s\n", msg );
-                }
-                else if ( opcode == -2 )
-                    printf( "Bad request line encountered.  Expected 4 valid "
-                              "tokens.  Message discarded.\n" );
-                return;
-            }
-            status = 0;
-        }
-        else if ( r != -1 )
-        {
-            opcode = RTSP_last_request + 100;
-            if ( status > 202 )
-                printf( "Response had status = %d.\n", status );
-        }
-        else
-            return;
-        */
-        handle_event( opcode, status, buffer ); //server.c:915
+        memcpy (buffer, in_buffer, llen); // 먼저 buffer 에 복사
+        opcode = validate_method(buffer); //parse.c:371 읽은 RTSP를 분석한다. 
+        handle_event(opcode, status, buffer); //server.c:915
     }
 }
 
@@ -135,4 +95,44 @@ io_write_pending()
 {
     return out_size;
 }
+
+int
+validate_method( char *s )
+{
+    TKN *m;
+    char directive [32];
+    char host [256];
+    char version [32];
+    int pcnt; /* parameter count */
+
+    *directive = *host = '\0';
+
+    /* pcnt = 3 이면 RTSP 첫번째 줄을 나타냄 */
+    pcnt = sscanf( s, "%31s %255s %31s", directive, host, version);
+    
+    if (pcnt != 3)
+    {
+        discard_msg(); /* remove remainder of message from in_buffer */
+        return(-2);
+    }
+    
+
+    /* 어떤 Directive인지 찾는다 */
+    for (m = Methods; m->code != -1; m++)
+        if (!strcmp( m->token, directive ))
+            break;
+
+    // 현재 m 은 해당 token 을 가지고 있음
+    
+    
+    if ( m->code == -1 )
+    {
+        discard_msg(); /* remove remainder of message from in_buffer */
+        printf("No Existed Directive.\n");
+    }
+
+    printf("%s, %d \n", s, m->code); // 읽은 한줄이랑 코드네임 출력해본다.
+    return( m->code ); //opcode 리턴
+}
+
 
